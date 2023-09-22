df0e9f31cf05ddcf47184fc0867a0dfb1a3f8074
(Student) - Danny Delaney <c00286125@setu.ie>
2023-09-22 10:16:24 +0100

    Q1 complete

---
+++ Lab Exercise 1/Lab Exercise 1/Lab1q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+	int array[10] = { 0,1,2,3,4,5,6,7,8,9, };
+	int* ptr = array;
+	size_t arrayBytes = sizeof(array);
+	size_t elementBytes = sizeof(int);
+	size_t noOfElements = arrayBytes / elementBytes;
+	for (int i = 0; i < noOfElements; i++)
+	{
+		std::cout << "element" << i << "=" << ptr[i] << "\n";
+	}
+}
